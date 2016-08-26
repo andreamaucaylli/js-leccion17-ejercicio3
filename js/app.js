4cb94a8ba27bf0b5e68161cdab4f25a84b432ff4
codingirlcode <dopamint@hotmail.com>
2016-08-26 10:50:53 -0500

    html, js, readme

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var num =parseInt( prompt("Ingrese un número entero"));
+
+if(num%2 != 0){
+  window.alert("Odd!");
+}
+else if(num%2 == 0 &&  num>2 &&  num<5){
+	window.alert("Good!");
+}
+else if(num%2 == 0 && num>6 && numero<20){
+	window.alert("Great!");
+}
+else if(numero%2 == 0 && numero>20){
+	window.alert("Perfect!");
+}
+
